76f84fe8fe41be827a197662f649cd219005bfc0
조용진 <cyj@joyongjin-ui-MacBookPro.local>
2026-02-14 22:33:59 +0900

    battsafe v1.7.7

--- Casks/battsafe.rb
+++ Casks/battsafe.rb
@@ -1,8 +1,8 @@
 cask "battsafe" do
-  version "1.7.6"
-  sha256 "c363590e39bf987af5e25561c8aed7b1f9eb8f703d19375e08d25e5e0c952446"
+  version "1.7.7"
+  sha256 "a7ba0b78163c40b9f37978949378066793368402fdc409cc6fd3519c8bb753c8"
 
-  url "https://github.com/zerry-lab/battsafe-releases/releases/download/v1.7.6/BattSafe.zip"
+  url "https://github.com/zerry-lab/battsafe-releases/releases/download/v1.7.7/BattSafe.zip"
   name "BattSafe"
   desc "Battery charge limit manager for Apple Silicon Macs"
   homepage "https://github.com/zerry-lab/battsafe-releases"
